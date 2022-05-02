be6a56f7192556e8fa7fee1a3ab17bac8d2313f6
danopike <danopike@gmail.com>
2022-05-02 19:37:10 +0100

    Initial Sign Up
    Adds validation for each field

--- app/src/main/java/com/danpike/socialapp/SignUpFragment.kt
+++ app/src/main/java/com/danpike/socialapp/SignUpFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.danpike.socialapp.databinding.FragmentSignUpBinding
 
@@ -24,7 +27,9 @@ class SignUpFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
+
         _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
         return binding.root
 
     }
@@ -32,9 +37,54 @@ class SignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val firstNameEditText = binding.fragmentSignupFirstNameInput
+        val firstNameValidation = binding.fragmentSignupFirstNameValidation
+        val lastNameEditText = binding.fragmentSignupLastNameInput
+        val lastNameValidation = binding.fragmentSignupLastNameValidation
+        val emailEditText = binding.fragmentSignupEmailInput
+        val emailValidation = binding.fragmentEmailValidation
+        val passwordEditText = binding.fragmentSignupPasswordInput
+        val passwordValidation = binding.fragmentPasswordValidation
+        val confirmPasswordEditText = binding.fragmentSignupConfirmPasswordInput
+        val confirmPasswordValidation = binding.fragmentConfirmPasswordValidation
+        val signUpButton = binding.fragmentSignupSignUpButton
+
         binding.fragmentSignupBackButton.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
+        fun checkIsBlankValidation(text: EditText, validationMessage: TextView) {
+            if (text.text.isBlank()) {
+                validationMessage.visibility = View.VISIBLE
+            } else {
+                validationMessage.visibility = View.INVISIBLE
+            }
+        }
+
+        signUpButton.setOnClickListener {
+            checkIsBlankValidation(firstNameEditText, firstNameValidation)
+            checkIsBlankValidation(lastNameEditText, lastNameValidation)
+
+            if (!emailEditText.text.contains("@")) {
+                emailValidation.visibility = View.VISIBLE
+            } else {
+                emailValidation.visibility = View.INVISIBLE
+            }
+
+            if (passwordEditText.text.isBlank() || passwordEditText.text.length < 8) {
+                passwordValidation.visibility = View.VISIBLE
+            } else {
+                passwordValidation.visibility = View.INVISIBLE
+            }
+
+            if (passwordEditText.text != confirmPasswordEditText.text) {
+                confirmPasswordValidation.visibility = View.VISIBLE
+            } else {
+                confirmPasswordValidation.visibility = View.INVISIBLE
+            }
+        }
+
+
     }
 
     override fun onDestroyView() {
